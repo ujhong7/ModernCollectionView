fb7c5184019dff0efa6b1115fea3325acadd81bc
유재홍 <ujhong7@gmail.com>
2024-09-14 05:37:26 +0900

    파일생성

---
+++ ModernCollectionView/ModernCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ModernCollectionView
+//
+//  Created by yujaehong on 9/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
