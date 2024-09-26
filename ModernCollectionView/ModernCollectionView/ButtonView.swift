34206e8693d005d97a30e6bd105a1a164c3ca51f
유재홍 <ujhong7@gmail.com>
2024-09-27 02:15:59 +0900

    ButtonView 생성

--- ModernCollectionView/ModernCollectionView/ButtonView.swift
+++ ModernCollectionView/ModernCollectionView/ButtonView.swift
@@ -5,8 +5,49 @@
 //  Created by yujaehong on 9/14/24.
 //
 
-import Foundation
+import UIKit
+import SnapKit
 
 class ButtonView: UIView {
+    
+    let tvButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("TV", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.configuration = UIButton.Configuration.bordered()
+        return button
+    }()
+    
+    let movieButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Movie", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.configuration = UIButton.Configuration.bordered()
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setUI() {
+        self.addSubview(tvButton)
+        self.addSubview(movieButton)
+        
+        tvButton.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalTo(20)
+        }
+        
+        movieButton.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalTo(tvButton.snp.trailing).offset(10)
+        }
+    }
     
 }
