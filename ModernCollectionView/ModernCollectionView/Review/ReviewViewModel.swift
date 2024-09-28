1cbb39fbb7c35d862b3711194aa6d5f7a8ecc562
유재홍 <ujhong7@gmail.com>
2024-09-28 15:45:30 +0900

    ReviewViewModel 생성

--- ModernCollectionView/ModernCollectionView/Review/ReviewViewModel.swift
+++ ModernCollectionView/ModernCollectionView/Review/ReviewViewModel.swift
@@ -5,4 +5,35 @@
 //  Created by yujaehong on 9/27/24.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+
+final class ReviewViewModel {
+    private let reviewNetwork: ReviewNetwork
+    private let id: Int, contentType: ContentType
+    
+    init(id: Int, contentType: ContentType) {
+        let provider = NetworkProvider()
+        self.reviewNetwork = provider.makeReviewNetwork()
+        self.id = id
+        self.contentType = contentType
+    }
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        let reviewResult: Observable<Result<[ReviewModel],Error>>
+    }
+    
+    func transform(input: Input) -> Output {
+        let reviewResult: Observable<Result<[ReviewModel],Error>> = reviewNetwork.getReviewList(id: id, contentType: contentType).map { reviewResult in
+            return .success(reviewResult.results)
+        }.catchError { error in
+            return Observable.just(.failure(error))
+        }
+        return Output(reviewResult: reviewResult)
+    }
+    
+}
